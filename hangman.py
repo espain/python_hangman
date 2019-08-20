125fc3308db7a698a1075b08ea41fb3666db0dcc
Ellie S <42261218+espain@users.noreply.github.com>
2019-08-19 23:21:49 -0500

    Add files via upload
    First draft of program. I just got an extension to help me with syntax, so I'll edit that next so it can actually run.

---
+++ hangman.py
@@ -0,0 +1,97 @@
+﻿#Ask for number of attempts, make sure it is between 1 and 25, inclusive
+while triesValid == false:
+ print("How many tries would you like to guess the word? Pick a number from 1 to 25.")
+ tries = input()
+ if tries >= 1 and tries <= 25:
+  triesValid = true
+  continue
+ else:
+  triesValid = false
+
+#Ask for minimum word length, make sure it is between 4 and 16, inclusive [I will explain this later.]
+while lengthValid == false:
+ print("How long would you like your word to be? Pick a number from 4 to 16.")
+ length = input()
+ if length >= 4 and length <= 16:
+  lengthValid = true
+  continue
+ else:
+  lengthValid = false
+
+#Open the word list file & select a random word
+#fileOpen = open("C:\Users\ellie\OneDrive\Documents\Programming Practice\wordListUse.txt", "r")
+wordListUse = open("wordListUse.txt", "r").readlines()
+#use the length variable and random to find a word that is the right length
+#select that word and set it to the variable secretWord
+while secretWord == "0":
+  i = random.randint(0, 69321)
+  if len(wordListUse[i]) == length:
+  secretWord == wordListUse[i]
+
+#Create a set of remaining letters and initialize it to contain the 26 ASCII lowercase character
+lowerAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+remainingLettersSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+#Where my loop begins...
+#While there are attempts remaining OR there are unguessed letters in the word remaining
+#Print the word with the unguessed letters censored
+gameContinues = true
+  roundContinues = true
+  while roundContinues == true:
+  print("Please guess a letter.")
+  for secretWord[i]:
+    if secretWord[i] is in remainingLettersSet:
+      print(" _ ")
+    else:
+      print (secretWord[i])
+    #Ask for the next letter and make it lowercase
+    guessedLetter = input()
+    lowerGuess = lower(guessedLetter)
+    #If the "letter" has multiple characters Notify the player that the "letter" has multiple characters
+    if len(lowerGuess) > 1:
+    print("Please guess only one letter at a time.")
+    #Else if the letter is not an ASCII lowercase character Notify the player that the letter is not an ASCII lowercase character
+    elif lowerGuess is not in lowerAlphabet:
+    print("Please guess a letter.")
+    #Else if the letter is not in the remaining letter set (i.e. has been guessed before) Notify the player that the letter has been guessed before
+    elif lowerGuess is not in remainingLettersSet:
+    print("You have already guessed that letter. Please guess another letter.")
+    #Else If letter is in the word Notify the player that the letter is in the word
+    elif lowerGuess is in secretWord:
+    remainingLettersSet.remove(lowerGuess)
+    print("That is correct! You have ", tries, " tries left.")
+    #Else Decrement attempt counter Notify the player that the letter is not in the word
+    #Remove guessed letter from the remaining letter set
+    else:
+    remainingLettersSet.remove(lowerGuess)
+    tries = tries - 1
+    print("That is incorrect. You have ", tries, " tries left.")
+  if tries == 0:
+    roundContinues = false
+  for secretWord[i]:
+    if secretWord[i] is in remainingLettersSet:
+    continue
+    else roundContinues = false:
+
+  #Reveal the word If the word is solved Notify the player of victory
+  print("The secret word was ",secretWord,".")
+  #Else Notify the player of defeat
+  if tries != 0:
+    print("You won!")
+  else:
+    print("You lost!")
+
+  #Give the player the option to try again
+  questionLoop = true
+  while questionLoop == true:
+    print("Would you like to try again? Y/N")
+    continueGame = input()
+    if continueGame == Y:
+    gameContinues = true
+    questionLoop = false
+    elif continueGame == N:
+    print("Thank you for playing.")
+    gameContinues = false
+    questionLoop = false
+  else:
+   continue
